2ac1c306a653e717706751d1cf5a946b8d9441f3
Nadia Gill <nadia.gill2006@gmail.com>
2024-08-06 18:14:09 +0200

    Newsletter

--- src/components/objects/Newsletter.js
+++ src/components/objects/Newsletter.js
@@ -2,12 +2,36 @@ import { useState } from 'react';
 import '../../styles/styles/newsletter.scss';
 
 export default function Newsletter() {
-
+    const [isFormOpen, setFormOpen] = useState(false);
+    const [isSubmitted, setSubmitted] = useState(false);
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const handleNewsletterFormOpen = () => {
+        setFormOpen(!isFormOpen);
+    }
+    const handleNewsletterSubmit = () => {
+        setFormOpen(false);
+        setSubmitted(true);
+    }
     return (
         <div className='newsletter-wrapper'>
             <h2>Need more herb tips?</h2>
             <p>Have them delivered straight to your mailbox!</p>
-            <button className='unclicked'>I WANT TIPS!</button>
+            { !isSubmitted && !isFormOpen &&
+                <div className='button'>
+                    <button className='unclicked' onClick={handleNewsletterFormOpen}>I WANT TIPS!</button>
+                </div>
+            }
+            {isFormOpen &&
+                <div className='newsletter-form'>
+                    <form onSubmit={() => handleNewsletterSubmit()}>
+                        <input type='text' placeholder='Name' value={name} onChange={(e) => setName(e.target.value)}/>
+                        <input type='email' placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                        <button className='submit-button' type='submit'>Count me in!</button>
+                    </form>
+                </div>
+            }
+            {isSubmitted && <h2>THANK YOU!</h2>}
         </div>
     );
 }
